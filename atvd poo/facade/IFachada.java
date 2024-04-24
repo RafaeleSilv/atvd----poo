3debc2b76b4640646b675f29b21b4790da0647b0
rafshinee <116653186+rafshinee@users.noreply.github.com>
2024-04-24 15:21:17 -0300

    Update IFachada.java

--- atvd poo/facade/IFachada.java
+++ atvd poo/facade/IFachada.java
@@ -1,13 +1,13 @@
 package facade;
 
 import entity.Conta;
-import exception.ExcecaoElementoInexistente;
+import exception.ExcecaoElementoInex;
 
 public interface IFachada {
     public void inserirConta(Conta conta);
     public void alterarConta(String numero); 
     public void removerConta(String numero);
-    public Conta buscarConta(String numero) throws ExcecaoElementoInexistente ;
+    public Conta buscarConta(String numero) throws ExcecaoElementoInex ;
     public boolean verificarExistenciaConta(String numero) ;
     public void mostrarContas();
 }
